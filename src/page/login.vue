8b9be56bb788dfbb39416c0d9ee6be485082a954
wurong <chriswwu@163.com>
2019-07-20 18:14:48 +0800

    pro init

---
+++ src/page/login.vue
@@ -0,0 +1,35 @@
+<template>
+  	<div class="login_page fillcontain">
+	  <div>login页面</div>
+        <button @click="submit">login</button>
+  	</div>
+</template>
+
+<script>
+	import {login} from '@/api/getData'
+
+	export default {
+	    data(){
+			return {
+			}
+		},
+		mounted(){
+
+		},
+		computed: {
+
+		},
+		methods: {
+
+
+		},
+		watch: {
+
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+	@import '../style/mixin';
+
+</style>
